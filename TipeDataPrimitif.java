64c7d66df689dde12bd5a33359c4193e7a0aa375
Dies Nadi <nadi.selamet@gmail.com>
2021-11-07 20:14:40 +0700

    Variabel Java

---
+++ TipeDataPrimitif.java
@@ -0,0 +1,15 @@
+package JavaBasic;
+
+public class TipeDataPrimitif {
+
+    //Memiliki 8 data primitif
+    //boolean  {true or false}
+    //char     {single characted}
+    //byte     {integer}
+    //short    {integer}
+    //int      {integer}
+    //long     {integer}
+    //float    {floating-point number}
+    //double   {floating-point number}
+
+}
---
+++ Variable.java
@@ -0,0 +1,17 @@
+package JavaBasic;
+
+public class Variable {
+
+    public static void main (String args[]){
+        
+        //Tipe data:
+        int a = 1;
+        float b = 2;
+        boolean c = true;
+
+        System.out.print("\nVariable a = " + a);
+        System.out.print("\nVariable b = " + b);
+        System.out.print("\nVariable c = " + c);
+    
+    }
+}
