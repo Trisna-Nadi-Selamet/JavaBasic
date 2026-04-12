5ef296e3ddf2afa4001be9f29460fdb36c67d57b
Trisna-Nadi-Selamet <nadi.selamet@gmail.com>
2026-04-12 23:53:04 +0700

    for

---
+++ StatementFor.java
@@ -0,0 +1,13 @@
+public class StatementFor {
+
+    public static void main(String[] args) {
+
+        int a = 6;
+
+        // Syarat mutlak untuk penggunaan for yaitu TRUE.
+        for (int i = 0; i < a; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
