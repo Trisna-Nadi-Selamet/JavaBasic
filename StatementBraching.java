4579e6c10d66c7e38410147caee2544a7e2b6a64
Trisna-Nadi-Selamet <nadi.selamet@gmail.com>
2026-04-12 23:59:22 +0700

    Bercabang Penggelangan

---
+++ StatementBraching.java
@@ -0,0 +1,12 @@
+public class StatementBraching {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            if (i == 4) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
